574246e521f38d9b48fdb84d0e6fddb5f48dfce9
ItIsSunny385 <ema.minoru@gmail.com>
2021-05-26 03:21:07 +0000

    環境変数を一部 .env.local に移動 close #24

--- chizu-manager-firebase/src/next.config.js
+++ chizu-manager-firebase/src/next.config.js
@@ -1,18 +1,16 @@
-const functions = require('firebase-functions')
-
 module.exports = {
     distDir: '../.next',
     env: {
-        debug: functions.config().debug.debug,
-        apiKey: functions.config().project_config.api_key,
-        authDomain: functions.config().project_config.auth_domain,
-        projectId: functions.config().project_config.project_id,
-        storageBucket: functions.config().project_config.storage_bucket,
-        messagingSenderId: functions.config().project_config.messaging_sender_id,
-        appId: functions.config().project_config.app_id,
-        googleMapsApiKey: functions.config().google_maps.api_key,
-        googleMapsCenterLat: functions.config().google_maps.center.lat,
-        googleMapsCenterLng: functions.config().google_maps.center.lng,
-        googleMapsZoom: functions.config().google_maps.zoom,
+        debug: process.env.FIREBASE_DEBUG,
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        googleMapsApiKey: process.env.GOOGLEMAPS_API_KEY,
+        googleMapsCenterLat: process.env.GOOGLE_MAPS_CENTER_LAT,
+        googleMapsCenterLng: process.env.GOOGLE_MAPS_CENTER_LNG,
+        googleMapsZoom: process.env.GOOGLE_MAPS_ZOOM,
     }
 }
