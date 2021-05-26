8d6a46b735a88189ccc5287bad9672ced1d996f7
ItIsSunny385 <ema.minoru@gmail.com>
2021-05-26 13:05:45 +0000

    favicon を設定 close #26

--- chizu-manager-firebase/src/components/App.tsx
+++ chizu-manager-firebase/src/components/App.tsx
@@ -24,6 +24,15 @@ export default function App(props: Props) {
         <React.Fragment>
             <Head>
                 <title>{props.title} | 地図マネージャ</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+                <link rel="manifest" href="/favicons/site.webmanifest" />
+                <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+                <link rel="shortcut icon" href="/favicons/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <main style={props.containerStyle}>
                 <Header
